0e144633032569809ca82a45d00d3d616228e8db
khovann <lordkovan@gmail.com>
2020-12-15 09:07:26 +0100

    Day 1

---
+++ day1/index.js
@@ -0,0 +1,28 @@
+var fs = require('fs')
+
+// get input
+let cleanList = fs.readFileSync(__dirname + '/input.txt', 'utf-8').split('\n').filter(item => item).map(Number);
+
+function star1(list) {
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < list.length; j++) {
+            if ((list[i] + list[j]) == 2020) {
+                return list[i] * list[j];
+            }
+        }
+    }
+}
+
+function star2(list) {
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < list.length; j++) {
+            for (let k = 0; k < list.length; k++) {
+                if ((list[i] + list[j] + list[k]) == 2020) {
+                    return list[i] * list[j] * list[k];
+                }
+            }
+        }
+    }
+}
+exports.s1 = star1(cleanList)
+exports.s2 = star2(cleanList)
